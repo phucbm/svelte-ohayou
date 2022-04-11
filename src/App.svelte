7a44016eaaf7acb3332b1ffd571fb32f23cb5a2f
phucbm <hiphucbui@gmail.com>
2022-04-11 09:49:25 +0700

    each loop

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,25 @@
 <script>
-    // A # character always indicates a block opening tag. A / character always indicates a block closing tag. A : character, as in {:else},
-    // indicates a block continuation tag.
-
-
-    let user = {loggedIn: false};
-
-    function toggle(){
-        user.loggedIn = !user.loggedIn;
-    }
-
-    let x = 7;
+    let cats = [
+        {id: 'J---aiyznGQ', name: 'Keyboard Cat'},
+        {id: 'z_AbfPXTKms', name: 'Maru'},
+        {id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat'}
+    ];
 </script>
 
-<code>
-</code>
+<h1>The Famous Cats of YouTube</h1>
 
-{#if user.loggedIn}
-    <button on:click={toggle}>
-        Log out
-    </button>
-{:else}
-    <button on:click={toggle}>
-        Log in
-    </button>
-{/if}
+<ul>
+    {#each cats as cat,i}
+        <li><a target="_blank" href="https://www.youtube.com/watch?v={cat.id}">
+            {cat.name}
+        </a></li>
+    {/each}
+</ul>
 
-
-{#if x > 10}
-    <p>{x} is greater than 10</p>
-{:else if 5 > x}
-    <p>{x} is less than 5</p>
-{:else}
-    <p>{x} is between 5 and 10</p>
-{/if}
+<ul>
+    {#each cats as {id, name}}
+        <li><a target="_blank" href="https://www.youtube.com/watch?v={id}">
+            {name}
+        </a></li>
+    {/each}
+</ul>
