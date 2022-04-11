5a3e81a64467475afc61af83864f12376ec0f07f
phucbm <hiphucbui@gmail.com>
2022-04-11 09:35:27 +0700

    declaring prop

---
+++ src/Nested.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let answer;
+    // That's not how export normally works in JavaScript modules! Just roll with it for now — it'll soon become second nature.
+</script>
+
+<p>The answer is {answer}</p>
