b0051971900b7efaa378ae70d6ff15e82087a5bb
phucbm <hiphucbui@gmail.com>
2022-04-11 09:45:51 +0700

    if else blocks

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,35 @@
 <script>
-    import Info from './Info.svelte';
+    // A # character always indicates a block opening tag. A / character always indicates a block closing tag. A : character, as in {:else},
+    // indicates a block continuation tag.
 
-    const pkg = {
-        name: 'svelte',
-        version: 3,
-        speed: 'blazing',
-        website: 'https://svelte.dev',
-        undeclaredValue: 'Undeclared value!',
-    };
+
+    let user = {loggedIn: false};
+
+    function toggle(){
+        user.loggedIn = !user.loggedIn;
+    }
+
+    let x = 7;
 </script>
 
-<Info {...pkg}/>
+<code>
+</code>
+
+{#if user.loggedIn}
+    <button on:click={toggle}>
+        Log out
+    </button>
+{:else}
+    <button on:click={toggle}>
+        Log in
+    </button>
+{/if}
+
+
+{#if x > 10}
+    <p>{x} is greater than 10</p>
+{:else if 5 > x}
+    <p>{x} is less than 5</p>
+{:else}
+    <p>{x} is between 5 and 10</p>
+{/if}
