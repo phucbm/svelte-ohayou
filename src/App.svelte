df300baeadc7466e18be6c9b885e93a579b6b611
phucbm <hiphucbui@gmail.com>
2022-04-11 09:24:15 +0700

    :sparkles: re-run this code whenever any of the referenced values change

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,15 @@
 <script>
-	export let name;
+    let count = 0;
+    $: doubled = count * 2;
+
+    function incrementCount(){
+        // event handler code goes here
+        count += 1;
+    }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<button on:click={incrementCount}>
+    Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<p>{count} doubled is {doubled}</p>
