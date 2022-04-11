4d0b60351004917046bd0821e38d0e4b1a1ef2ec
phucbm <hiphucbui@gmail.com>
2022-04-11 09:41:34 +0700

    spread props with undeclared value

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,13 @@
 <script>
-    import Nested from './Nested.svelte';
+    import Info from './Info.svelte';
+
+    const pkg = {
+        name: 'svelte',
+        version: 3,
+        speed: 'blazing',
+        website: 'https://svelte.dev',
+        undeclaredValue: 'Undeclared value!',
+    };
 </script>
 
-<Nested/>
+<Info {...pkg}/>
