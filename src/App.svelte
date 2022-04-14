f0a14d79244718d091727aa49d100316ee8686ae
phucbm <hiphucbui@gmail.com>
2022-04-14 12:22:29 +0700

    bindings: each block bindings

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,57 @@
 <script>
-    let html = '<p>Write some text!</p>';
+    let todos = [
+        {done: false, text: 'finish Svelte tutorial'},
+        {done: false, text: 'build an app'},
+        {done: false, text: 'world domination'}
+    ];
+
+    function add(){
+        todos = todos.concat({done: false, text: ''});
+    }
+
+    function clear(){
+        todos = todos.filter(t => !t.done);
+    }
+
+    function remove(index){
+        const arr = [...todos];
+        arr.splice(index, 1)
+
+        todos = arr;
+    }
+
+    $: remaining = todos.filter(t => !t.done).length;
 </script>
 
-<div contenteditable="true" bind:innerHTML={html}></div>
+<h1>Todos</h1>
 
-<pre>{html}</pre>
+{#each todos as todo,index}
+    <div class:done={todo.done}>
+        <input
+                type=checkbox
+                bind:checked={todo.done}
+        >
+
+        <input
+                placeholder="What needs to be done?"
+                bind:value={todo.text}
+        >
+        <button type="button" on:click={()=>remove(index)}>remove</button>
+    </div>
+{/each}
+
+<p>{remaining} remaining</p>
+
+<button on:click={add}>
+    Add new
+</button>
+
+<button on:click={clear}>
+    Clear completed
+</button>
 
 <style>
-    [contenteditable] {
-        padding:0.5em;
-        border:1px solid #eee;
-        border-radius:4px;
+    .done {
+        opacity:0.4;
     }
 </style>
