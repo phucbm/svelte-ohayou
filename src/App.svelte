9a7e37d6cd4e7276a4eea55ee8e17f9dff6dae9a
phucbm <hiphucbui@gmail.com>
2022-04-14 14:55:28 +0700

    bindings: component bindings

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,14 @@
 <script>
-    let w;
-    let h;
-    let size = 42;
-    let text = 'edit me';
+    import Keypad from './Keypad.svelte';
+
+    let pin;
+    $: view = pin ? pin.replace(/\d(?!$)/g, '•') : 'enter your pin';
+
+    function handleSubmit(){
+        alert(`submitted ${pin}`);
+    }
 </script>
 
-<input type=range bind:value={size}>
-<input bind:value={text}>
+<h1 style="color: {pin ? '#333' : '#ccc'}">{view}</h1>
 
-<p>size: {w}px x {h}px</p>
-
-<div bind:clientWidth={w} bind:clientHeight={h}>
-    <span style="font-size: {size}px">{text}</span>
-</div>
-
-<style>
-    input { display:block; }
-    div { display:inline-block; }
-    span { word-break:break-all; }
-</style>
+<Keypad bind:value={pin} on:submit={handleSubmit}/>
